7bb5d2f17b80542ca36bd41aecf4d201e0ba4a22
dannyboywoop <danieljholmes@hotmail.co.uk>
2020-07-12 15:12:41 +0000

    Replaced single quotes with double quotes for strings

--- lambda/AlexaResponse.py
+++ lambda/AlexaResponse.py
@@ -3,7 +3,7 @@ import time
 
 
 def get_utc_timestamp(seconds=None):
-    return time.strftime('%Y-%m-%dT%H:%M:%S.00Z', time.gmtime(seconds))
+    return time.strftime("%Y-%m-%dT%H:%M:%S.00Z", time.gmtime(seconds))
 
 
 class AlexaResponse:
@@ -16,33 +16,33 @@ class AlexaResponse:
         # Set up the response structure
         self.context = {}
         self.event = {
-            'header': {
-                'namespace': kwargs.get('namespace', 'Alexa'),
-                'name': kwargs.get('name', 'Response'),
-                'messageId': str(uuid.uuid4()),
-                'payloadVersion': kwargs.get('payload_version', '3')
+            "header": {
+                "namespace": kwargs.get("namespace", "Alexa"),
+                "name": kwargs.get("name", "Response"),
+                "messageId": str(uuid.uuid4()),
+                "payloadVersion": kwargs.get("payload_version", "3")
             },
-            'endpoint': {
+            "endpoint": {
                 "scope": {
                     "type": "BearerToken",
-                    "token": kwargs.get('token', 'INVALID')
+                    "token": kwargs.get("token", "INVALID")
                 },
-                "endpointId": kwargs.get('endpoint_id', 'INVALID')
+                "endpointId": kwargs.get("endpoint_id", "INVALID")
             },
-            'payload': kwargs.get('payload', {})
+            "payload": kwargs.get("payload", {})
         }
 
-        if 'correlation_token' in kwargs:
-            self.event['header']['correlation_token'] =\
-                kwargs.get('correlation_token', 'INVALID')
+        if "correlation_token" in kwargs:
+            self.event["header"]["correlation_token"] =\
+                kwargs.get("correlation_token", "INVALID")
 
-        if 'cookie' in kwargs:
-            self.event['endpoint']['cookie'] = kwargs.get('cookie', '{}')
+        if "cookie" in kwargs:
+            self.event["endpoint"]["cookie"] = kwargs.get("cookie", "{}")
 
         # No endpoint in an AcceptGrant or Discover request
-        if (self.event['header']['name'] == 'AcceptGrant.Response'
-           or self.event['header']['name'] == 'Discover.Response'):
-            self.event.pop('endpoint')
+        if (self.event["header"]["name"] == "AcceptGrant.Response"
+           or self.event["header"]["name"] == "Discover.Response"):
+            self.event.pop("endpoint")
 
     def add_context_property(self, **kwargs):
         self.context_properties.append(self.create_context_property(**kwargs))
@@ -59,74 +59,74 @@ class AlexaResponse:
 
     def create_context_property(self, **kwargs):
         return {
-            'namespace': kwargs.get('namespace', 'Alexa.EndpointHealth'),
-            'name': kwargs.get('name', 'connectivity'),
-            'value': kwargs.get('value', {'value': 'OK'}),
-            'timeOfSample': get_utc_timestamp(),
-            'uncertaintyInMilliseconds': kwargs.get(
-                'uncertainty_in_milliseconds', 0)
+            "namespace": kwargs.get("namespace", "Alexa.EndpointHealth"),
+            "name": kwargs.get("name", "connectivity"),
+            "value": kwargs.get("value", {"value": "OK"}),
+            "timeOfSample": get_utc_timestamp(),
+            "uncertaintyInMilliseconds": kwargs.get(
+                "uncertainty_in_milliseconds", 0)
         }
 
     def create_payload_endpoint(self, **kwargs):
         # Return the proper structure expected for the endpoint
         endpoint = {
-            'capabilities': kwargs.get('capabilities', []),
-            'description': kwargs.get('description', 'Custom Smart Device'),
-            'displayCategories': kwargs.get('display_categories', ['OTHER']),
-            'endpointId': kwargs.get('endpoint_id', 'endpoint_0'),
-            'friendlyName': kwargs.get('friendly_name', 'NULL'),
-            'manufacturerName': kwargs.get('manufacturer_name',
-                                           'Tanno Holmes Ideas')
+            "capabilities": kwargs.get("capabilities", []),
+            "description": kwargs.get("description", "Custom Smart Device"),
+            "displayCategories": kwargs.get("display_categories", ["OTHER"]),
+            "endpointId": kwargs.get("endpoint_id", "endpoint_0"),
+            "friendlyName": kwargs.get("friendly_name", "NULL"),
+            "manufacturerName": kwargs.get("manufacturer_name",
+                                           "Tanno Holmes Ideas")
         }
 
-        if 'cookie' in kwargs:
-            endpoint['cookie'] = kwargs.get('cookie', {})
+        if "cookie" in kwargs:
+            endpoint["cookie"] = kwargs.get("cookie", {})
 
         return endpoint
 
     def create_payload_endpoint_capability(self, **kwargs):
         capability = {
-            'type': kwargs.get('type', 'AlexaInterface'),
-            'interface': kwargs.get('interface', 'Alexa'),
-            'version': kwargs.get('version', '3')
+            "type": kwargs.get("type", "AlexaInterface"),
+            "interface": kwargs.get("interface", "Alexa"),
+            "version": kwargs.get("version", "3")
         }
-        supported = kwargs.get('supported', None)
+        supported = kwargs.get("supported", None)
         if supported:
-            capability['properties'] = {}
-            capability['properties']['supported'] = supported
-            capability['properties']['proactivelyReported'] =\
-                kwargs.get('proactively_reported', True)
-            capability['properties']['retrievable'] =\
-                kwargs.get('retrievable', True)
+            capability["properties"] = {}
+            capability["properties"]["supported"] = supported
+            capability["properties"]["proactivelyReported"] =\
+                kwargs.get("proactively_reported", True)
+            capability["properties"]["retrievable"] =\
+                kwargs.get("retrievable", True)
         return capability
 
     def get(self, remove_empty=True):
 
         response = {
-            'context': self.context,
-            'event': self.event
+            "context": self.context,
+            "event": self.event
         }
 
         if len(self.context_properties) > 0:
-            response['context']['properties'] = self.context_properties
+            response["context"]["properties"] = self.context_properties
 
         if len(self.payload_endpoints) > 0:
-            response['event']['payload']['endpoints'] = self.payload_endpoints
+            response["event"]["payload"]["endpoints"] = self.payload_endpoints
 
         if remove_empty:
-            if len(response['context']) < 1:
-                response.pop('context')
+            if len(response["context"]) < 1:
+                response.pop("context")
 
         return response
 
     def set_payload(self, payload):
-        self.event['payload'] = payload
+        self.event["payload"] = payload
 
     def set_payload_endpoint(self, payload_endpoints):
         self.payload_endpoints = payload_endpoints
 
     def set_payload_endpoints(self, payload_endpoints):
-        if 'endpoints' not in self.event['payload']:
-            self.event['payload']['endpoints'] = []
+        if "endpoints" not in self.event["payload"]:
+            self.event["payload"]["endpoints"] = []
 
-        self.event['payload']['endpoints'] = payload_endpoints
+        self.event["payload"]["endpoints"] = payload_endpoints
--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -5,119 +5,119 @@ from AlexaResponse import AlexaResponse
 def lambda_handler(request, context):
 
     # Dump the request for logging - check the CloudWatch logs
-    print('lambda_handler request  -----')
+    print("lambda_handler request  -----")
     print(json.dumps(request))
 
     if context is not None:
-        print('lambda_handler context  -----')
+        print("lambda_handler context  -----")
         print(context)
 
     # Validate we have an Alexa directive
-    if 'directive' not in request:
+    if "directive" not in request:
         aer = AlexaResponse(
-            name='ErrorResponse',
-            payload={'type': 'INVALID_DIRECTIVE',
-                     'message': 'Missing key: directive, ' +
-                     'Is the request a valid Alexa Directive?'})
+            name="ErrorResponse",
+            payload={"type": "INVALID_DIRECTIVE",
+                     "message": "Missing key: directive, " +
+                     "Is the request a valid Alexa Directive?"})
         return send_response(aer.get())
 
     # Check the payload version
-    payload_version = request['directive']['header']['payloadVersion']
-    if payload_version != '3':
+    payload_version = request["directive"]["header"]["payloadVersion"]
+    if payload_version != "3":
         aer = AlexaResponse(
-            name='ErrorResponse',
-            payload={'type': 'INTERNAL_ERROR',
-                     'message': 'This skill only supports Smart Home API v3.'})
+            name="ErrorResponse",
+            payload={"type": "INTERNAL_ERROR",
+                     "message": "This skill only supports Smart Home API v3."})
         return send_response(aer.get())
 
     # Crack open the request and see what is being requested
-    name = request['directive']['header']['name']
-    namespace = request['directive']['header']['namespace']
+    name = request["directive"]["header"]["name"]
+    namespace = request["directive"]["header"]["namespace"]
 
     # Handle the incoming request from Alexa based on the namespace
 
-    if namespace == 'Alexa.Authorization':
-        if name == 'AcceptGrant':
+    if namespace == "Alexa.Authorization":
+        if name == "AcceptGrant":
             # Note: This sample accepts any grant request
-            grant_code = request['directive']['payload']['grant']['code']
-            grantee_token = request['directive']['payload']['grantee']['token']
+            grant_code = request["directive"]["payload"]["grant"]["code"]
+            grantee_token = request["directive"]["payload"]["grantee"]["token"]
             print(grant_code)
             print(grantee_token)
-            aar = AlexaResponse(namespace='Alexa.Authorization',
-                                name='AcceptGrant.Response')
+            aar = AlexaResponse(namespace="Alexa.Authorization",
+                                name="AcceptGrant.Response")
             return send_response(aar.get())
 
-    if namespace == 'Alexa.Discovery':
-        if name == 'Discover':
-            adr = AlexaResponse(namespace='Alexa.Discovery',
-                                name='Discover.Response')
+    if namespace == "Alexa.Discovery":
+        if name == "Discover":
+            adr = AlexaResponse(namespace="Alexa.Discovery",
+                                name="Discover.Response")
             capability_alexa = adr.create_payload_endpoint_capability()
             capability_alexa_percentagecontroller =\
                 adr.create_payload_endpoint_capability(
-                    interface='Alexa.PercentageController',
-                    supported=[{'name': 'percentage'}])
+                    interface="Alexa.PercentageController",
+                    supported=[{"name": "percentage"}])
             adr.add_payload_endpoint(
-                friendly_name='Blinds',
-                endpoint_id='smart_blind_01',
-                description='Custom Smart Blind',
-                display_categories=['INTERIOR_BLIND'],
+                friendly_name="Blinds",
+                endpoint_id="smart_blind_01",
+                description="Custom Smart Blind",
+                display_categories=["INTERIOR_BLIND"],
                 capabilities=[capability_alexa,
                               capability_alexa_percentagecontroller])
             return send_response(adr.get())
 
-    if namespace == 'Alexa.PercentageController':
-        endpoint_id = request['directive']['endpoint']['endpointId']
-        correlation_token = request['directive']['header']['correlationToken']
-        token = request['directive']['endpoint']['scope']['token']
+    if namespace == "Alexa.PercentageController":
+        endpoint_id = request["directive"]["endpoint"]["endpointId"]
+        correlation_token = request["directive"]["header"]["correlationToken"]
+        token = request["directive"]["endpoint"]["scope"]["token"]
 
-        if name == 'SetPercentage':
-            percentage = request['directive']['payload']['percentage']
+        if name == "SetPercentage":
+            percentage = request["directive"]["payload"]["percentage"]
             state_set = set_device_state(endpoint_id=endpoint_id,
-                                         state='percentage',
+                                         state="percentage",
                                          value=percentage)
             final_percentage = percentage
 
-        if name == 'AdjustPercentage':
-            percentDelta = request['directive']['payload']['percentageDelta']
+        if name == "AdjustPercentage":
+            percentDelta = request["directive"]["payload"]["percentageDelta"]
             state_set = set_device_state(endpoint_id=endpoint_id,
-                                         state='percentageDelta',
+                                         state="percentageDelta",
                                          value=percentDelta)
             final_percentage = 50
 
         # Check for an error when setting the state
         if not state_set:
             return AlexaResponse(
-                name='ErrorResponse',
-                payload={'type': 'ENDPOINT_UNREACHABLE',
-                         'message': 'Unable to reach endpoint database.'}
+                name="ErrorResponse",
+                payload={"type": "ENDPOINT_UNREACHABLE",
+                         "message": "Unable to reach endpoint database."}
             ).get()
 
         apcr = AlexaResponse(correlation_token=correlation_token,
                              endpoint_id=endpoint_id,
                              token=token)
-        apcr.add_context_property(namespace='Alexa.PercentageController',
+        apcr.add_context_property(namespace="Alexa.PercentageController",
                                   name="percentage",
                                   value=final_percentage)
         return send_response(apcr.get())
 
-    if namespace == 'Alexa' and name == 'ReportState':
-        endpoint_id = request['directive']['endpoint']['endpointId']
-        correlation_token = request['directive']['header']['correlationToken']
-        token = request['directive']['endpoint']['scope']['token']
+    if namespace == "Alexa" and name == "ReportState":
+        endpoint_id = request["directive"]["endpoint"]["endpointId"]
+        correlation_token = request["directive"]["header"]["correlationToken"]
+        token = request["directive"]["endpoint"]["scope"]["token"]
 
         state = get_device_state(endpoint_id)
         if state is None:
             return AlexaResponse(
-                name='ErrorResponse',
-                payload={'type': 'ENDPOINT_UNREACHABLE',
-                         'message': 'Unable to reach endpoint database.'}
+                name="ErrorResponse",
+                payload={"type": "ENDPOINT_UNREACHABLE",
+                         "message": "Unable to reach endpoint database."}
             ).get()
 
         apcr = AlexaResponse(correlation_token=correlation_token,
                              endpoint_id=endpoint_id,
                              token=token,
-                             name='StateReport')
-        apcr.add_context_property(namespace='Alexa.PercentageController',
+                             name="StateReport")
+        apcr.add_context_property(namespace="Alexa.PercentageController",
                                   name="percentage",
                                   value=state)
         return send_response(apcr.get())
@@ -125,7 +125,7 @@ def lambda_handler(request, context):
 
 def send_response(response):
     # TODO Validate the response
-    print('lambda_handler response -----')
+    print("lambda_handler response -----")
     print(json.dumps(response))
     return response
 
